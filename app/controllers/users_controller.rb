5f05da3c317ab55f67fbe83b20a42ff01f771876
darealstyl <jonahball5@hotmail.com>
2025-11-25 14:17:24 -0500

    signup link on home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "User was created succesfully."
+      flash[:notice] = "User was created succesfully. Welcome to the Alpha-Blog #{@user}"
       redirect_to @user
     else
       render :new, status: :unprocessable_entity
