ecc4cb2cde1575617c0838ffd6573d46771aa0b2
darealstyl <jonahball5@hotmail.com>
2025-12-14 21:55:29 -0500

    added conditional login/logout buttons to navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "User was created succesfully. Welcome to the Alpha-Blog #{@user}"
+      session[:user_id] = @user.id
+      flash[:notice] = "User was created succesfully. Welcome to the Alpha-Blog #{@user.username}"
       redirect_to @user
     else
       render :new, status: :unprocessable_entity
