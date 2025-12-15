7f8fed332787deb009cbde1cd6ef4b1144688eef
darealstyl <jonahball5@hotmail.com>
2025-12-14 21:32:59 -0500

    added login authentication and logout funtionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in successfully"
+      redirect_to user
+    else
+      flash.now[:alert] = "There was something wrong with your login details"
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    user = session[:user_id] = nil
+    flash[:notice] = "Logged out successfully"
+    redirect_to root_path
   end
 end
