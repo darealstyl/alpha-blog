a614ae779f91ea87096bf3819b2874cc102eb230
darealstyl <jonahball5@hotmail.com>
2025-12-16 15:21:06 -0500

    categories controller and tests

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :articles
   get "signup", to: "users#new"
   resources :users, except: [:new]
+  resources :categories, except: [:destroy]
 
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
