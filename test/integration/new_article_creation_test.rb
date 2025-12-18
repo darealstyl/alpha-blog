a67bf096d6bc978127a464b27d84f7312054956e
darealstyl <jonahball5@hotmail.com>
2025-12-17 19:22:53 -0500

    Update new_article_creation_test.rb

--- test/integration/new_article_creation_test.rb
+++ test/integration/new_article_creation_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 class NewArticleCreationTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @regular_user = User.create(username: "John", email: "johndoe@example.com", password: "asd123", admin: false)
+    @category = Category.create(name: "Sports")
+  end
+
+  test "get the new article form and create article" do
+    sign_in_as(@regular_user)
+    get "/articles/new"
+    assert_response :success
+    assert_difference "Article.count", 1 do
+      post articles_path, params: { article: { title: "i like sports", description: "sports are the best!", categories: ["Sports"] } }
+      assert_response :redirect
+    end
+    follow_redirect!
+    assert_response :success
+    assert_match "i like sports", response.body
+  end
 end
