f30584e6cdc202c6b4b7d0d805dacaec78fd0702
darealstyl <jonahball5@hotmail.com>
2025-12-17 19:18:49 -0500

    Update user_sign_up_test.rb

--- test/integration/user_sign_up_test.rb
+++ test/integration/user_sign_up_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class UserSignUpTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "get the signup page and create a new user" do
+    get "/signup"
+    assert_response :success
+    assert_difference "User.count", 1 do
+      post users_path, params: { user: { username: "john", email: "johndoee@example.com", password: "asd123" } }
+      assert_response :redirect
+    end
+    follow_redirect!
+    assert_response :success
+    assert_match "john's profile", response.body
+  end
 end
